62ffc2ebdadc1d5d7f9820952bb90783dc4f4636
adiabdulrozaq <adiabdulrozaq15@gmail.com>
2026-02-25 14:23:49 +0700

    Pesan failed pada Progres Pembangunan KNMP Nasional & UI filter by date, dan Delete fitur lupa password

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,6 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.perform')->middleware('throttle:5,1');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Password Reset Routes
-Route::get('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
-
 
 Route::middleware('auth')->group(function () {
 
