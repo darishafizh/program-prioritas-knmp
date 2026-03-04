9202046ca784ba44d430c0589f317ac40a178c5d
adiabdulrozaq <adiabdulrozaq15@gmail.com>
2026-03-04 10:19:59 +0700

    Cleaning in script & Fixing UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FormsController;
